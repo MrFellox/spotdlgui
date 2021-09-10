69d03ee35956a701df66a9d91e646875c3c14e7f
MrFellox <65425622+MrFellox@users.noreply.github.com>
2021-09-10 10:46:45 -0600

    User can now choose where to save a song
    Added more functions to utils.py file
    Changes + code improvement in main.py

--- spotdlgui/core/spotdlgui/scripts/utils.py
+++ spotdlgui/core/spotdlgui/scripts/utils.py
@@ -1,5 +1,9 @@
 import json
-default_settings = {"isMaximized": False}
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+default_settings = {"isMaximized": False, "askLocation": True, 'defaultLocation': None}
 
 def get_window_state() -> bool:
     '''
@@ -40,3 +44,58 @@ def create_config_file():
     '''
     Creates/overwrites the config file with the default settings.
     '''
+    os.mkdir('./core/data')
+    with open('./core/data/config.json' 'w') as f:
+        json.dump(default_settings, f, indent=2)
+        
+def get_switch_setting(setting):
+    '''
+    'Gets the value of the config.json of the specified setting and returns the correct value for Kivy
+    '''
+    with open('./core/data/config.json') as f:
+        data = json.load(f)
+
+    return data[setting]
+
+def change_config_value(setting, value):
+    '''
+    Modifies the specified setting for the specified value 
+    '''
+    
+    with open('./core/data/config.json') as f:
+        data = json.load(f)
+
+        data[setting] = value
+
+    with open('./core/data/config.json', 'w') as f:
+        json.dump(data, f, indent=2)
+
+def get_setting_value(setting):
+    '''
+    Returns the value of the specified setting
+    '''
+    with open('./core/data/config.json') as f:
+        data = json.load(f)
+
+    return data[setting]
+
+def get_download_path() -> str:
+    '''
+    Returns the path to the selected folder where the user wants to save songs.
+    '''
+    if get_setting_value('askLocation') == True:
+        output = ''
+
+        # Using tk since it uses system's ui.
+        root = tk.Tk()
+        root.withdraw()
+        
+        while output == '':
+            output = filedialog.askdirectory()
+        
+    else:
+        # Get Desktop path
+        output = os.path.join(os.path.join(
+            os.environ['USERPROFILE']), 'Desktop')
+
+    return output
--- spotdlgui/main.py
+++ spotdlgui/main.py
@@ -1,5 +1,6 @@
-from core.spotdlgui.scripts import utils as app_utils
 from kivy.app import App
+from kivy.lang import Builder
+from core.spotdlgui.scripts import utils as app_utils
 from kivy.config import Config
 
 # Disable option to exit the app when Esc is pressed.
@@ -8,23 +9,23 @@ Config.set('kivy', 'exit_on_escape', '0')
 # Open app depending if it was maximized or not.
 if app_utils.get_window_state() == True:
     Config.set('graphics', 'window_state', 'maximized')
-else:
-    Config.set('graphics', 'width', '800')
-    Config.set('graphics', 'height', '500')
 
-
-from kivy.lang import Builder
-from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.core.text import LabelBase
+from kivy.properties import BooleanProperty
+from kivy.uix.screenmanager import Screen, ScreenManager
 from core.spotdlgui.scripts.popups import DownloadingPopup
 from spotdl.download import DownloadManager
 from spotdl.parsers.query_parser import parse_query
 from spotdl.search import SpotifyClient
-import threading
-import time
 import os
+import time
+import ctypes
+import threading
 
+# Set high dpi
+
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 # Add fonts
 
@@ -41,36 +42,44 @@ for font in KIVY_FONTS:
     LabelBase.register(**font)
 
 
-class SpotDLGUI(App):
-    def build(self):
-        self.title = 'SpotDLGUI v1.0'
-
-        # Initialize a Spotify Client
-
-        SpotifyClient.init(
-            client_id="5f573c9620494bae87890c0f08a60293",
-            client_secret="212476d9b0f3472eaa762d90b19b0ba8",
-            user_auth=False
-        )
-
-        # Event listeners
-        Window.bind(on_maximize=self.on_maximize)
-        Window.bind(on_restore=self.on_minimize)
+# Initialize a Spotify client
+# ! Client is initalized here to avoid any initalization error.
+SpotifyClient.init(
+    client_id="5f573c9620494bae87890c0f08a60293",
+    client_secret="212476d9b0f3472eaa762d90b19b0ba8",
+    user_auth=False
+)
 
 
-        return Builder.load_file('core/spotdlgui/designs/ui.kv')
+class Manager(ScreenManager):
+    pass
+
+
+class MainScreen(Screen):
+    # Ran when settings button is clicked.
+    def _enter_settings(self):
+        '''
+        This function runs when the "Settings" option is clicked,
+        showing an animation to the settings screen.
+        '''
+        self.manager.current = 'optionsScreen'
+        self.manager.transition.direction = 'left'
 
     def download_song(self):
-        # Get search query from the text entry
-        search_query = App.get_running_app().root.ids['searcher'].text
+        '''
+        This function downloads the list of the song specified and shows progress of download in string.
+        '''
 
-        # Get Desktop path
-        desktop = os.path.join(os.path.join(
-            os.environ['USERPROFILE']), 'Desktop')
+        self.output_path = app_utils.get_download_path()
+
+        # Get search query from the text entry
+        # ! Replaced from App.get_running_app().ids['searcher'].text because we're now using screens.
+        search_query = self.ids['searcher'].text
 
         self.popup.change_text('Searching song(s)...')
 
-        song_list = parse_query(
+        # Parse songs
+        self.song_list = parse_query(
             [search_query],
             'mp3',
             False,
@@ -78,53 +87,102 @@ class SpotDLGUI(App):
             1
         )
 
-        for song_obj in song_list:
-            # Change popup text to current song
-            
-            self.popup.change_text(f'Attempting to download \n"{song_obj.song_name}" by {song_obj.contributing_artists[0]}...')
-            
-            # Save current directory.
+        # Save app path
 
-            app_path = os.path.dirname(os.path.realpath(__file__))
+        app_path = os.path.dirname(os.path.realpath(__file__))
 
-            # Move to desktop to save songs there
-            os.chdir(desktop)
+        # Move to the output path
 
-            # Download
+        os.chdir(self.output_path)
+
+        for song in self.song_list:
+            # Change popup text
+
+            self.popup.change_text(
+                f'Attempting to download \n"{song.song_name}" by {song.contributing_artists[0]}...')
 
             with DownloadManager() as downloader:
-                downloader.download_single_song(song_obj)
-            
-            # Move to the app path
+                downloader.download_single_song(song)
 
-            os.chdir(app_path)
-
-            self.popup.change_text(f'Succesfully downloaded \n"{song_obj.song_name}" to Desktop.')
+            self.popup.change_text(
+                f'Succesfully downloaded \n"{song.song_name}" to ' + self.output_path)
             time.sleep(3)
 
         # Clean text on entry
 
-        App.get_running_app().root.ids['searcher'].text = ''
-        
+        self.ids['searcher'].text = ''
+
+        # Move to the app path
+
+        os.chdir(app_path)
+
         # Close popup
         self.popup.dismiss()
 
     def _on_download_press(self):
-        # Show popup
-
+        '''
+        Runs when donwload button is pressed
+        '''
+        # Start an instance of the progress popup and show it
         self.popup = DownloadingPopup()
         self.popup.open()
 
-        thread = threading.Thread(target=self.download_song)
-        thread.start()
+        # Start download thread.
+        download_thread = threading.Thread(target=self.download_song)
+        download_thread.start()
 
+
+class OptionsScreen(Screen):
+    # ! on_pre_enter is fired when the screen is about to be used: the entering animation is started.
+    # ! so we use pre enter so it doesn't see the switches turn position by themselves.
+
+    def on_pre_enter(self):
+        # Update switch positions
+        ask_location_switch = self.ids['askLocation_switch']
+        ask_location_switch.active = app_utils.get_switch_setting(
+            'askLocation')
+
+        # Bind all switches to switch callback function
+        self.ids['askLocation_switch'].bind(active=self.switch_callback)
+
+    def switch_callback(self, instance, value):
+        '''
+        Runs everytime a switch that is linked to this callback is switched.
+        '''
+        if instance == 'askLocation':
+            app_utils.change_config_value('askLocation', value)
+
+        else:
+            pass
+
+
+class SpotDLGUI(App):
+    askLocation = BooleanProperty(app_utils.get_switch_setting('askLocation'))
+
+    def build(self):
+        self.title = 'SpotDLGUI v1.0'
+
+        # Event listeners
+        Window.bind(on_maximize=self.on_maximize)
+        Window.bind(on_restore=self.on_minimize)
+
+        return Builder.load_file('core/spotdlgui/designs/ui.kv')
 
     # Kivy events
     def on_maximize(self, *args):
-        app_utils.change_saved_window_state(True)
+        try:
+            app_utils.change_saved_window_state(True)
+
+        except FileNotFoundError:
+            pass
 
     def on_minimize(self, *args):
-        app_utils.change_saved_window_state(False)
+        try:
+            app_utils.change_saved_window_state(False)
+
+        except FileNotFoundError:
+            pass
+
 
 if __name__ == '__main__':
     SpotDLGUI().run()
